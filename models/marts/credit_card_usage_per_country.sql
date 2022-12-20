3c6946929b56f44a55467c8c007e762488dc5c6c
Luan Moreno <luan.moreno@owshq.com>
2022-12-20 19:06:15 +0000

    payments_per_plan_credit_card_type

--- models/marts/credit_card_usage_per_country.sql
+++ models/marts/credit_card_usage_per_country.sql
@@ -1,10 +1,10 @@
-with users as (
+with cleansed_user as (
 
     select * from {{ ref('cleansed_user') }}
 
 ),
 
-payments as (
+cleansed_payments as (
 
     select * from {{ ref('cleansed_payments') }}
 
@@ -13,8 +13,8 @@ payments as (
     credit_card_usage_per_country as (
 
         select ps.country, ps.currency, ps.credit_card_type, count(*) as amount
-        from users as pu
-        inner join payments as ps using(user_id)
+        from cleansed_user as pu
+        inner join cleansed_payments as ps using(user_id)
         group by ps.country, ps.currency, ps.credit_card_type
     )
 
